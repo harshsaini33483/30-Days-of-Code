a32b9e2a779fdf1b0176c0017b8f0897d72731d3
Harsh-Saini <vinaysaini97@gmail.com>
2019-10-21 03:38:21 +0530

    Day 24

--- ShirtPack.java
+++ ShirtPack.java
@@ -9,23 +9,24 @@ class ShirtPack{
             int m=sc.nextInt();
             if(n+1==m){
                 System.out.println(1);
-            }
+            }else if((n%2==0 && m%2==0) || n==m){
+                System.out.println(-1);
+            } 
            else{
-            int count=0,mD=m,nD=n;
-            while(true){
-                if(nD+1==mD){
-                    System.out.println(count);
-                    break;
-                }
-                else if(nD>mD){
-                    mD=mD+m;
-                     ++count;
-                }else{
-                    nD=nD+n;
-                }
-               
-            }
-        }
+                System.out.println(hardWay(n, m));
+             }
            } 
+    }
+    public static int hardWay(int n,int m){
+        int i=1;
+        while(i<10000){
+            double temp0=(((double)n*i)+1)/(double)m;
+            int temp1=((n*i)+1)/m;
+            if(temp1-temp0==0.0){
+                return temp1;
+            }
+            ++i;
+        }
+        return -1;
     }
 }
